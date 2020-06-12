acd6c752224cd18232f0a12a28e617380938eb31
Daniel Hahm <dhahm01@gmail.com>
2020-06-12 01:52:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,9 @@
 # Write your code here!
+
+def game_hash
+  #return a hash nested :home, :away 
+game = {}
+game[:home=>{:team_name=>"TEAM NAME HERE", :colors=>["colors listed here"], :players=>{}}]
+game[:away=>{:team_name=>"TEAM NAME HERE", :colors=>["colors listed here"], :players=>{}}]
+  
+end
